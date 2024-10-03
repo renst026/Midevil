cc1b47cded999aae9dd77b559966b4d2b7e5dbe0
Regan Gross <gros0591@d.umn.edu>
2024-10-03 16:02:05 -0500

    Fixed animation for walking and added idle. alwso added a tree

--- scenes/Knight.gd
+++ scenes/Knight.gd
@@ -1,25 +1,33 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
-
 func _physics_process(delta: float) -> void:
+	 #Walking animation 
+	if (velocity.x > 1 || velocity.x < -1):
+		$Sprite2D.animation = "walking"
+	else:
+		$Sprite2D.animation = "idle" 
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 20)
 
 	move_and_slide()
+	
+	if Input.is_action_just_pressed('ui_left'):
+		$Sprite2D.flip_h = true
+	if Input.is_action_just_pressed('ui_right'):
+		$Sprite2D.flip_h = false
